fc860b77ab3822a62a09381de4b3c6f5c5c60ec3
ivan-paz <ivnpaz@gmail.com>
2021-10-18 21:56:45 +0200

    some comments

--- session4/Example_3/Main.java
+++ session4/Example_3/Main.java
@@ -10,9 +10,10 @@ class Main {
   // DECLARA  H E R E the type of exception
   public static void findFile() throws IOException {
 
-    // code that may generate IOException
-    File newFile = new File("/home/ivan/documents/prop/session4/Example_3/test.txt");
-    //File newFile = new File("/home/ivan/documents/prop/session4/Example_3/text.txt");
+    // code that MAY generate IOException
+    // para evitar la Exception el archivo debe exsistir . . . obvio
+    //File newFile = new File("/home/ivan/documents/prop/session4/Example_3/test.txt");
+    File newFile = new File("/home/ivan/documents/prop/session4/Example_3/text.txt");
     FileInputStream stream = new FileInputStream(newFile);
   }
 
